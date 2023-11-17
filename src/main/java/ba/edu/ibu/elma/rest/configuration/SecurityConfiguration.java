2d0b8ec2e7227dd51aed10b8370a8dd2354a0d0a
elma <elma.sarajlic@stu.ibu.edu.ba>
2023-11-17 22:01:54 +0100

    configuration

--- src/main/java/ba/edu/ibu/elma/rest/configuration/SecurityConfiguration.java
+++ src/main/java/ba/edu/ibu/elma/rest/configuration/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package ba.edu.ibu.elma.rest.configuration;
 
 import ba.edu.ibu.elma.core.service.UserService;
 import ba.edu.ibu.elma.rest.filters.JwtAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -22,25 +21,26 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserService userService;
 
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
-    @Autowired
-    private UserService userService;
+    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter, UserService userService) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.userService = userService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.GET, "/api/books/**").permitAll()
-                        .requestMatchers("/api/books/**").authenticated()
-                        .requestMatchers("/api/users/**").authenticated()
-                        .anyRequest().permitAll())
+        http.csrf().disable()
+                .authorizeRequests(request -> request
+                        .antMatchers(HttpMethod.GET, "/api/users/**").permitAll()
+                )
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
--- src/main/java/ba/edu/ibu/elma/rest/controllers/UserController.java
+++ src/main/java/ba/edu/ibu/elma/rest/controllers/UserController.java
@@ -1,63 +1,63 @@
 package ba.edu.ibu.elma.rest.controllers;
 
-import ba.edu.ibu.elma.core.model.enums.UserType;
+import ba.edu.ibu.elma.core.model.User;
 import ba.edu.ibu.elma.core.service.UserService;
 import ba.edu.ibu.elma.rest.dto.UserDTO;
 import ba.edu.ibu.elma.rest.dto.UserRequestDTO;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
-@RequestMapping("/users")
+@RequestMapping("api/users")
+@SecurityRequirement(name = "JWT Security")
 public class UserController {
+
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @RequestMapping(path = "/register", method = RequestMethod.POST)
-    public ResponseEntity<UserDTO> register(@RequestBody UserRequestDTO userRequestDTO) {
-        return ResponseEntity.ok(userService.register(userRequestDTO));
+
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    @PreAuthorize("hasAnyAuthority('LIBRARIAN', 'ADMIN')")
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
     }
 
-    @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public ResponseEntity<UserDTO> login(@RequestParam String username, @RequestParam String password) {
-        return ResponseEntity.ok(userService.login(username, password));
+    @RequestMapping(method = RequestMethod.POST, path = "/register")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<UserDTO> register(@RequestBody UserRequestDTO user) {
+        return ResponseEntity.ok(userService.addUser(user));
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    @PreAuthorize("hasAnyAuthority('LIBRARIAN', 'ADMIN')")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable String id) {
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 
-    @RequestMapping(path = "/{userId}", method = RequestMethod.GET)
-    public ResponseEntity<UserDTO> getUserById(@PathVariable String userId) {
-        return ResponseEntity.ok(userService.getUserById(userId));
+    @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @RequestBody UserRequestDTO user) {
+        return ResponseEntity.ok(userService.updateUser(id, user));
     }
 
-    @RequestMapping(path = "/{userId}", method = RequestMethod.PUT)
-    public ResponseEntity<UserDTO> updateUser(@PathVariable String userId, @RequestBody UserRequestDTO userRequestDTO) {
-        return ResponseEntity.ok(userService.updateUser(userId, userRequestDTO));
-    }
-
-    @RequestMapping(path = "/{userId}", method = RequestMethod.DELETE)
-    public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
-        userService.deleteUser(userId);
-        return ResponseEntity.noContent().build();
+    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/filter")
+    @PreAuthorize("hasAnyAuthority('LIBRARIAN', 'ADMIN')")
     public ResponseEntity<UserDTO> filterUser(@RequestParam String email) {
         return ResponseEntity.ok(userService.filterByEmail(email));
-    }
-
-    @RequestMapping(value = "/{userId}/type", method = RequestMethod.PUT)
-    public ResponseEntity<Void> updateType(@PathVariable String userId, @RequestParam UserType type) {
-        userService.updateType(userId, type);
-        return ResponseEntity.noContent().build();
     }
 }
