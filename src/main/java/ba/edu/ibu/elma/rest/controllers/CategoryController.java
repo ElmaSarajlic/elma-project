b1b3179daeb84fea1b1413dc6f2839041c8921ac
elma <elma.sarajlic@stu.ibu.edu.ba>
2023-11-18 02:31:33 +0100

    security

--- src/main/java/ba/edu/ibu/elma/rest/controllers/CategoryController.java
+++ src/main/java/ba/edu/ibu/elma/rest/controllers/CategoryController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class CategoryController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
+    @PreAuthorize("hasAnyAuthority( 'ADMIN')")
     public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryRequestDTO categoryRequestDTO) {
         CategoryDTO createdCategory = categoryService.createCategory(categoryRequestDTO);
         return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
