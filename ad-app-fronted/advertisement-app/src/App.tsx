d0709ebd46cdf0ab2897579b4e96441e64062f71
elma <elma.sarajlic@stu.ibu.edu.ba>
2023-12-23 01:50:41 +0100

    register

--- ad-app-fronted/advertisement-app/src/App.tsx
+++ ad-app-fronted/advertisement-app/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import NavBar from './components/NavBar'
 import Login from './components/login'
+import Register from './components/register'
 
 function App() {
   return (
     <>
-      {<Login /> }
+      {/*<Login/> */ }
+      {<NavBar /> }
+
     </>
   )
 }
--- ad-app-fronted/advertisement-app/src/components/NavBar/NavBar.tsx
+++ ad-app-fronted/advertisement-app/src/components/NavBar/NavBar.tsx
@@ -1,46 +1,55 @@
+// Header.tsx
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Header: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Your App</Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" to="/login">Login</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Category
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><Link className="dropdown-item" to="#">Action</Link></li>
-                <li><Link className="dropdown-item" to="#">Another action</Link></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><Link className="dropdown-item" to="#">Something else here</Link></li>
-              </ul>
+              <Link className="nav-link" to="/register">Register</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
+              <Link className="nav-link" to="/logout">Logout</Link>
             </li>
           </ul>
-          <form className="d-flex">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
         </div>
+
+        <form className="form-inline">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
       </div>
     </nav>
   );
 };
 
-export default Navbar;
+export default Header;
