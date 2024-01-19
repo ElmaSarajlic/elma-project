309d47c524bd9dcf7346effe1a227c5f06c0a773
elma <elma.sarajlic@stu.ibu.edu.ba>
2024-01-20 00:00:37 +0100

    deletebutton

--- ad-app-fronted/advertisement-app/src/components/UserCard/UserCard.tsx
+++ ad-app-fronted/advertisement-app/src/components/UserCard/UserCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, Avatar, Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Avatar, Typography, IconButton } from '@mui/material';
 import useGetUsers from '../../hooks/useGetUsers';
 import { User } from '../../utils/types';
+import DeleteBtn from '../DeleteBtn';
 
 type Props = {
     user: User;
@@ -14,8 +15,14 @@ const UserCard = ({ user }: Props) => {
       <Card sx={{ marginBottom: 2, width: '100%' }}> 
         <CardHeader
           avatar={<Avatar src={user.imgUrl} alt={user.username} />}
-          title={user.username  DeleteButton onDelete={onDelete} } 
+          title={user.username } 
           subheader={user.email}
+          action={
+            <IconButton onClick={handleDelete} aria-label="delete user">
+              <DeleteBtn />
+            </IconButton>
+          }
+          sx={{ '& .MuiCardHeader-action': { alignSelf: 'center' } }}
         />
       </Card>
     );
