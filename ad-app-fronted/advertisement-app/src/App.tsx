0e787243540eeee29e612d10a316c7befa2e12e2
elma <elma.sarajlic@stu.ibu.edu.ba>
2024-01-19 22:45:17 +0100

    usercardlistpage

--- ad-app-fronted/advertisement-app/src/App.tsx
+++ ad-app-fronted/advertisement-app/src/App.tsx
@@ -62,7 +62,7 @@ function App() {
 /*export default App;*/
 
 import { Route, Routes } from "react-router-dom"
-import { Home, Login, Register, UserInfo, Categories, AdForm, EditUser, AdSubcategory, CreateCategory } from "./pages"
+import { Home, Login, Register, UserInfo, Categories, AdForm, EditUser, AdSubcategory, CreateCategory, UserList } from "./pages"
 import NavBar from "./components/NavBar"
 import UpdateAdForm from "./pages/UpdateAdForm"
 //import ProtectedRoute from "./utils/ProtectedRoute"
@@ -84,7 +84,9 @@ function App() {
         <Route path="/AdSubcategory" element={<AdSubcategory />} />
         <Route path="/subcategory/:subcategoryName" element={<Home />} />
         <Route path="/edit/:adId" element = {<UpdateAdForm />} />
-        <Route path="/createcategories/" element = {<CreateCategory />} />
+        <Route path="/createcategories" element = {<CreateCategory />} />
+        <Route path="/userList" element = {<UserList />} />
+
 
 
       </Routes>
--- ad-app-fronted/advertisement-app/src/pages/UserList.tsx
+++ ad-app-fronted/advertisement-app/src/pages/UserList.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { User } from '../utils/types';
 import UserCardList from '../components/UserCardList';
-import AppNavbar from '../components/NavBar';
+import NavBar from '../components/NavBar';
+import { Container } from 'react-bootstrap';
+import HomePage from './Home';
 
-interface HomePageProps {
-  users: User[]; // An array of User objects
-}
 
-const HomePage: React.FC<HomePageProps> = ({ users }) => {
-  return (
-    <div>
-      <AppNavbar />
-      <div style={{ padding: '20px' }}>
-        <h1>Welcome to the User List Page</h1>
-        <UserCardList users={users} />
-      </div>
-    </div>
-  );
-};
+const UserListPage: React.FC = () => {
+    return (
+        <><NavBar /><Container style={{ marginTop: '100px' }}>
+            <UserCardList users={[]} />
+        </Container></>
+    );
+  };
 
-export default HomePage;
+export default UserListPage;
