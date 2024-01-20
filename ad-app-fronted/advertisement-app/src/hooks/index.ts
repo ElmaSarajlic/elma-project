4a991ce2b18c46ed48d2686c02d7f24232d1e8d8
elma <elma.sarajlic@stu.ibu.edu.ba>
2024-01-20 02:10:54 +0100

    index

--- ad-app-fronted/advertisement-app/src/hooks/index.ts
+++ ad-app-fronted/advertisement-app/src/hooks/index.ts
@@ -7,7 +7,7 @@ export {default as useDeleteAd} from './useDeleteAd'
 export {default as useUpdateAd} from './useUpdateAd'
 export {default as useCreateCategory} from './useCreateCategory'
 export {default as useCreateSubcategory} from './useCreateSubcategory'
-export {default as useDeleteSubcategory} from '/useDeleteSubcategory'
+export {default as useDeleteSubcategory} from './useDeleteSubcategory'
 
 
 
